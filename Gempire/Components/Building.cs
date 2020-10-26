9769735fec768e06743a736e7ab9706e82a5c98f
Jonathan Kline <jonphilk@gmail.com>
2020-10-26 17:24:26 -0400

    Add project files.

---
+++ Gempire/Components/Building.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gempire.Components
+{
+    public class Building
+    {
+        public string Name { get; set; }
+        public Attribute Type { get; set; }
+        public int BaseValue { get; set; }
+        public VariableType VariableType { get; set; }
+        public int VariableCount { get; set; }
+        public int VariableConstant { get; set; }
+        public int TotalValue
+        {
+            get
+            {
+                return BaseValue + VariableConstant * VariableCount;
+            }
+        }
+        public Direction Direction { get; set; }
+        public int BuildingCount { get; set; }
+        public int PalaceDiamonds { get; set; }
+        public int ScienceSymbols { get; set; }
+        public int ScienceVariableValue { get; set; }
+
+
+    }
+
+    public enum Attribute
+    {
+        Gold,
+        Population,
+        Culture
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right
+    }
+
+    public enum VariableType
+    {
+        Building,
+        PalaceLevel,
+        GoldGem,
+        PopulationGem,
+        CultureGem
+    }
+}
---
+++ Gempire/Components/HybridBuilding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gempire.Components
+{
+    public class HybridBuilding : Building
+    {
+        public Attribute SecondType { get; set; }
+        public int SecondBaseValue { get; set; }
+        public VariableType SecondVariableType { get; set; }
+        public int SecondVariableConstant { get; set; }
+        public int SecondVariableCount { get; set; }
+        public int SecondTotalValue
+        {
+            get
+            {
+                return SecondBaseValue + SecondVariableConstant * SecondVariableCount;
+            }
+        }
+        public Direction SecondDirection { get; set; }
+        public int SecondBuildingCount { get; set; }
+    }
+}
