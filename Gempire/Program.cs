9f74acf91e7a1375470b75dc9d2c1c5ef83e4a2b
jonphilk <jonphilk@gmail.com>
2020-10-27 10:32:47 -0500

    test read and write to txt, CreateBuilding, GetBuildings

--- Gempire/Program.cs
+++ Gempire/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,28 @@ namespace Gempire
     {
         static void Main(string[] args)
         {
+            // Testing StreamWriter and StreamReader
+            string[] lines = { "First line", "Second line", "Third line" };
+            string path = @"C:\Users\jonph\Desktop\C#\Projects\Gempire\Gempire\Data\Test.txt";
+
+            foreach (var line in lines)
+            {
+                using (var file = new StreamWriter(path, true))
+                {
+                    file.WriteLine(line);
+                }
+            }
+
+            using (var file = new StreamReader(path))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                file.Close();
+            }
+            Console.ReadLine();
         }
     }
 }
--- Gempire/Repositories/BuildingRepository.cs
+++ Gempire/Repositories/BuildingRepository.cs
@@ -1,17 +1,44 @@
 ﻿using Gempire.Components;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Gempire.Repositories
 {
     public class BuildingRepository
     {
+        private readonly string _path = @"C:\Users\jonph\Desktop\C#\Projects\Gempire\Gempire\Data\Buildings.txt";
+
         public void CreateBuilding(Building building)
         {
+            string serializedBuilding = JsonConvert.SerializeObject(building);
+            using (var file =
+            new StreamWriter(_path, true))
+            {
+                file.WriteLine(serializedBuilding);
+            }
+        }
 
+        public List<Building> GetBuildings()
+        {
+            using (var file = new StreamReader(_path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                List<Building> buildings = new List<Building>();
+
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    Building deserializedBuilding = JsonConvert.DeserializeObject<Building>(line);
+                    buildings.Add(deserializedBuilding);
+                }
+
+                return buildings;
+            }
         }
     }
 }
